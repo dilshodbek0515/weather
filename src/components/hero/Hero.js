b1daac896a159871093758faaed7ded8d7d54f30
dilshod <dilshodx516@gmail.com>
2024-12-25 02:26:54 +0500

    weather

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,18 +3,16 @@ import Weather from '../weather/Weather'
 import LgWeather from '../lgWeather/LgWeather'
 
 const Hero = async () => {
-  // Weather
-  const res = await fetch(
+  const URL =
     'https://api.weatherapi.com/v1/forecast.json?key=cdd31fd04b3a4aac934203612242312&q=toshkent&days=10&aqi=yes&alerts=yes'
-  )
+  // Weather
+  const res = await fetch(URL)
   const posts = await res.json()
   const current = posts?.current
   const location = posts?.location
 
   // LgWeather
-  const data = await fetch(
-    'https://api.weatherapi.com/v1/forecast.json?key=cdd31fd04b3a4aac934203612242312&q=toshkent&days=10&aqi=yes&alerts=yes'
-  )
+  const data = await fetch(URL)
   const post = await data.json()
 
   return (
